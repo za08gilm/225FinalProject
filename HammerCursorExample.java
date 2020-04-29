5a8a0c2dd8f4acefe93ecccdf40cc5181d5b6fbe
za08gilm <za08gilm@siena.edu>
2020-04-29 12:51:31 -0400

    Fixed the cursor image for the HammerCursorExample class. - Zack

--- HammerCursorExample.java
+++ HammerCursorExample.java
@@ -25,7 +25,7 @@ public class HammerCursorExample implements Runnable {
             @Override public void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 
-                setBackground(Color.WHITE);
+                setBackground(Color.RED);
                 
                 Toolkit toolkit = Toolkit.getDefaultToolkit();
                 Image image = toolkit.getImage("mallet.png");
